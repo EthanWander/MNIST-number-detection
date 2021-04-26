635fdabf7f268258bb8a5bb5cf07742300ea8905
EthanWander <ethanwander@gmail.com>
2021-04-26 17:04:01 +0300

    main

--- main.py
+++ main.py
@@ -1,8 +1,62 @@
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 from tensorflow import keras
-import pprint as pp
+from tensorflow.keras import layers
+import math
 
-mnist = tf.keras.datasets.mnist.load_data(path="mnist.npz")
-pp.pprint(mnist[0][1][0])
-pp.pprint(mnist[0][0][0])
+
+ASCII_CHARS = [".", ",", ":", ";", "+", "*", "?", "%", "$", "#", "@"]
+
+
+def get_img_ascii(img):
+    ascii_str = ""
+    for line in img:
+        for pixel in line:
+            index = math.floor(len(ASCII_CHARS)*pixel/256)
+            ascii_str += ASCII_CHARS[index]
+        ascii_str += "\n"
+    return ascii_str
+
+
+def main():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+
+    x_train_norm = keras.utils.normalize(x_train, axis=1)
+    x_test_norm = keras.utils.normalize(x_test, axis=1)
+
+    model = keras.Sequential(
+        [
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(10, activation="softmax")
+        ]
+    )
+
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    model.fit(x_train_norm, y_train, batch_size=50, epochs=12, validation_split=0.2, verbose=1)
+
+    predictions = model.predict([x_test_norm])
+
+    print("\n\n")
+    print("Choose pictures out of the 10,000 available\n")
+
+    while True:
+        print("Enter an arbitrary number between 0 and 9999: ")
+        img_index = input()
+        img_index = int(img_index)
+        print("\n")
+        print(get_img_ascii(x_test[img_index]))
+        print(np.argmax(predictions[img_index]))
+        print("\n\n")
+
+
+if __name__ == "__main__":
+    main()
